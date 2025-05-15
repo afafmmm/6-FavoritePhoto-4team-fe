dffd287f76fb44a57ba979c9c2d86b677a744ea6
Wooju <dnwn810@naver.com>
2025-05-15 16:06:59 +0900

    feat: ui Title 추가

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,14 +1,21 @@
 "use client";
 import CardListPageEx from "@/components/FllterDropdown/CardListPageEx";
 import { Suspense } from "react"; //useSearchParams를 사용해서 Suspense로 감싸야 안전하게 CSR 처리가 됩니다.
-
 import Link from "next/link";
 import React from "react";
 
 export default function HomePage() {
+  const handleSellClick = () => alert("내 포토카드 판매하기 클릭");
   return (
     <div>
       HomePage
+        <Title 
+      title="마이페이지" 
+      buttonText="수정하기" 
+      onButtonClick={() => console.log("수정")} 
+      font="titleLg_Noto" 
+      buttonColor="yellow" 
+    />
       <Suspense fallback={null}>
         <CardListPageEx />
       </Suspense>
