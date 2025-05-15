f31a2f76eb8243a307e4395062e772151f792a97
Wooju <dnwn810@naver.com>
2025-05-15 11:03:19 +0900

    refactor: 리베이스 후 리팩토링 했습니다.음

---
+++ README.md
@@ -0,0 +1,2 @@
+### 리엑트 아이콘 추가
+ npm install react-icons --save
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,3 +1,4 @@
+import CardListPageEx from "@/components/FllterDropdown/CardListPageEx";//현재는 txt로 바뀜
 import Link from "next/link";
 import React from "react";
 
@@ -5,8 +6,10 @@ export default function HomePage() {
   return (
     <div>
       HomePage
+       
       <Link href="/signup">
-        <div>Input컴포넌트 확인용 페이지 클릭</div>
+        <div>Input컴포넌트 확인용 페이지 클릭
+        </div>
       </Link>
     </div>
   );
