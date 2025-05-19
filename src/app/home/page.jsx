3b61ec29a7f1f7932e2b12593a5e5d090505cb6d
az0319h <az0319h@naver.com>
2025-05-19 16:25:31 +0900

    홈 페이지 스켈레톤 추가함

--- src/app/home/page.jsx
+++ src/app/home/page.jsx
@@ -5,6 +5,7 @@ import { Suspense } from "react";
 export default function HomePage() {
   return ( 
       <div>
+        {/* 카드 영역 */}
         <div className="grid grid-cols-2 gap-4 md:gap-5 lg:grid-cols-3 lg:gap-20 ">
           <Suspense fallback={<HomeFallback count={15}/>}>
             <BaseCards />
