cebdc67c04b67ed4fc9a9d06e33b287f05d9b7ab
LEE JI SU <afafmm1903@gmail.com>
2025-05-23 18:39:44 +0900

    수정2

--- src/components/HompageComponents/BaseCardsSection.jsx
+++ src/components/HompageComponents/BaseCardsSection.jsx
@@ -6,13 +6,14 @@ import { Suspense } from "react";
 import HomeFallbackCount from "../skeletons/HomeFallbackCount";
 import NoResultMessage from "../ui/NoResultsMessage";
 
-
 export default async function BaseCardsSection({ grade, genre, sale }) {
   const API_BASE_URL = "https://six-favoritephoto-4team-be.onrender.com";
 
-  const res = await fetch(`${API_BASE_URL}/api/store`, { next: { revalidate: 60 } }); // 데이터 타임 설정
+  const res = await fetch(`${API_BASE_URL}/api/store`, {
+    next: { revalidate: 60 },
+  }); // 데이터 타임 설정
   if (!res.ok) {
-    throw new Error("카드 데이터를 불러오는데 실패했습니다.");
+    throw new Error("카드 데이터를 불러오는데 실패했습니다..");
   }
   const data = await res.json();
 
@@ -20,11 +21,12 @@ export default async function BaseCardsSection({ grade, genre, sale }) {
   const filtered = data.filter((card) => {
     const matchGrade = !grade || card.gradeId === Number(grade);
     const matchGenre = !genre || card.genreId === Number(genre);
-    const matchSale = !sale || (sale === "판매중" ? card.sale === "판매중" : card.sale === "판매완료");
+    const matchSale =
+      !sale ||
+      (sale === "판매중" ? card.sale === "판매중" : card.sale === "판매완료");
 
     return matchGrade && matchGenre && matchSale;
   });
-
 
   return (
     <>
@@ -43,18 +45,15 @@ export default async function BaseCardsSection({ grade, genre, sale }) {
           <Sort />
         </div>
       </div>
-      {
-        filtered.length === 0 ? (
-          <NoResultMessage message={"필터링 결과가 존재하지 않습니다."}/>
-        ) : (
-          <div className="grid grid-cols-2 lg:grid-cols-3 gap-4 md:gap-5 lg:gap-20">
-            <Suspense key={filtered} fallback={<HomeFallbackCount count={12} />}>
-              <BaseCardList cards={filtered} />
-            </Suspense>
-          </div>
-        )
-      }
-
+      {filtered.length === 0 ? (
+        <NoResultMessage message={"필터링 결과가 존재하지 않습니다."} />
+      ) : (
+        <div className="grid grid-cols-2 lg:grid-cols-3 gap-4 md:gap-5 lg:gap-20">
+          <Suspense key={filtered} fallback={<HomeFallbackCount count={12} />}>
+            <BaseCardList cards={filtered} />
+          </Suspense>
+        </div>
+      )}
     </>
   );
 }
